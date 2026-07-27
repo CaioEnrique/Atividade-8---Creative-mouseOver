588565a8eef11feb1625a190404ec80a38bd56cd
CaioEnrique <caio.lacerda@escola.pr.gov.br>
2026-07-27 11:52:40 -0300

    Delete script.js, style.css

--- script.js
+++
@@ -1,7 +0,0 @@
-let banner = document.querySelector('.banner');
-banner.onmousemove = function(e){
-    let x = e.pageX - banner.offsetLeft;
-    let y = e.pageY - banner.offsetTop;
-    banner.style.setProperty('--x', x + 'px');
-    banner.style.setProperty('--y', y + 'px');
-}
